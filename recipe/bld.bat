ed7e5002b9e68666d32385eb24f8e4f48896ea47
Ray Donnelly <mingw.android@gmail.com>
2018-08-13 06:38:32 -0500

    Oops

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,19 +1,11 @@
-echo on
-
 CALL npm install || EXIT /B 1
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
 
-CALL npm run clean || EXIT /B 1
-CALL npm run build || EXIT /B 1
 CALL npm run build:release || EXIT /B 1
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
-
-dir /s %SP_DIR%\nbpresent
-
-dir /s node_modules
 
 (rmdir /s /q "\\?\%cd%\node_modules" 2> NUL) || echo "some issues cleaning up"
 "%PYTHON%" setup.py install --single-version-externally-managed --record=record.txt
 
-CALL "%PREFIX%\Scripts\jupyter-nbextension" install nbpresent --py --sys-prefix --overwrite --log-level=0 || EXIT /B 1
+CALL "%PREFIX%\Scripts\jupyter-nbextension" install nbpresent --py --sys-prefix --overwrite || EXIT /B 1
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
